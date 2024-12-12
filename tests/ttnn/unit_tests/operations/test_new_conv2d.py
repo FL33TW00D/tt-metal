81585da2ffce018e8576f4c6341cf2b151f2fdbc
Pavle Josipovic <pjosipovic@tenstorrent.com>
2024-12-12 10:27:49 +0000

    test_dispnetres_conv

--- tests/ttnn/unit_tests/operations/test_new_conv2d.py
+++ tests/ttnn/unit_tests/operations/test_new_conv2d.py
@@ -2788,3 +2788,58 @@ def test_shallow_conv_with_tiled_input(device):
     passing, pcc_msg = check_with_pcc_without_tensor_printout(torch_output_tensor, torch_out_golden_tensor, pcc=0.99)
     logger.info(f"PCC = {pcc_msg}. Threshold = 0.99")
     assert passing
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size, output_channels, input_channels, input_height, input_width, filter_height, filter_width, stride_h, stride_w, pad_h, pad_w, weights_dtype, use_1d_systolic_array, config_override",
+    (
+        (1, 32, 11, 960, 576, 7, 7, 2, 2, 3, 3, ttnn.bfloat8_b, True, {"act_block_h": 32}),  # conv1
+        (1, 1, 32, 960, 576, 3, 3, 1, 1, 1, 1, ttnn.bfloat8_b, True, {"act_block_h": 32}),
+    ),
+)
+@pytest.mark.parametrize(
+    "activations_dtype",
+    [ttnn.bfloat8_b],
+)
+@pytest.mark.parametrize("math_fidelity", [ttnn.MathFidelity.LoFi])
+def test_dispnetres_conv(
+    device,
+    math_fidelity,
+    activations_dtype,
+    batch_size,
+    output_channels,
+    input_channels,
+    input_height,
+    input_width,
+    filter_height,
+    filter_width,
+    stride_h,
+    stride_w,
+    pad_h,
+    pad_w,
+    weights_dtype,
+    use_1d_systolic_array,
+    config_override,
+):
+    run_conv(
+        device,
+        math_fidelity,
+        activations_dtype,
+        weights_dtype,
+        batch_size,
+        output_channels,
+        input_channels,
+        input_height,
+        input_width,
+        filter_height,
+        filter_width,
+        stride_h,
+        stride_w,
+        pad_h,
+        pad_w,
+        use_1d_systolic_array,
+        config_override=config_override,
+        # output_layout=ttnn.ROW_MAJOR_LAYOUT,
+        auto_shard=True,
+    )
