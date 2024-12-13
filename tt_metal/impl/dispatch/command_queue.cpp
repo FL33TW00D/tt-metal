53a505125dd46ab30502db466af496fac89cf438
Kyle Mabee <kmabee@tenstorrent.com>
2024-12-13 00:18:07 +0000

    LightMetal - Add initial Trace/Replay support for many popular host APIs
     - Not comprehensive, just initial changes for some of these paths:
    
       CreateBuffer()
       EnqueueWriteBuffer()
       EnqueueReadBuffer()
       Finish()
       DeallocateBuffer
       ReleaseTrace()
       CreateProgram()
       EnqueueProgram()
       CreateKernel()
       SetRuntimeArgs(uint32)
       SetRuntimeArgs(Kernel,RuntimeArgs)
       CreateCircularBuffer()
    
     - When Metal Trace is enabled, don't capture EnqueueProgram(), instead
       inject ReplayTrace(), would be used alongside LoadTrace()
    
     - Serialization / Deserialization of structs/enums defined in
       flatbuffer schema types.fbs handled via ToFlatbuffer() and
       FromFlatbuffer() functions.

--- tt_metal/impl/dispatch/command_queue.cpp
+++ tt_metal/impl/dispatch/command_queue.cpp
@@ -2041,6 +2041,7 @@ void EnqueueReadBuffer(
     void* dst,
     bool blocking,
     tt::stl::Span<const SubDeviceId> sub_device_ids) {
+    TRACE_FUNCTION_CALL(CaptureEnqueueReadBuffer, cq, buffer, dst, blocking); // FIXME (kmabee) consider sub_device_ids added recently.
     detail::DispatchStateCheck(true);
     cq.run_command(CommandInterface{
         .type = EnqueueCommandType::ENQUEUE_READ_BUFFER, .blocking = blocking, .buffer = buffer, .dst = dst, .sub_device_ids = sub_device_ids});
@@ -2052,6 +2053,7 @@ void EnqueueWriteBuffer(
     HostDataType src,
     bool blocking,
     tt::stl::Span<const SubDeviceId> sub_device_ids) {
+    TRACE_FUNCTION_CALL(CaptureEnqueueWriteBuffer, cq, buffer, src, blocking); // FIXME (kmabee) consider sub_device_ids added recently.
     detail::DispatchStateCheck(true);
     cq.run_command(CommandInterface{
         .type = EnqueueCommandType::ENQUEUE_WRITE_BUFFER, .blocking = blocking, .buffer = buffer, .src = std::move(src), .sub_device_ids = sub_device_ids});
@@ -2059,6 +2061,7 @@ void EnqueueWriteBuffer(
 
 void EnqueueProgram(
     CommandQueue& cq, Program& program, bool blocking) {
+    TRACE_FUNCTION_CALL(CaptureEnqueueProgram, cq, program, blocking);
     detail::DispatchStateCheck(true);
     cq.run_command(
         CommandInterface{.type = EnqueueCommandType::ENQUEUE_PROGRAM, .blocking = blocking, .program = &program});
@@ -2120,6 +2123,7 @@ bool EventQuery(const std::shared_ptr<Event>& event) {
 }
 
 void Finish(CommandQueue& cq, tt::stl::Span<const SubDeviceId> sub_device_ids) {
+    TRACE_FUNCTION_CALL(CaptureFinish, cq); // FIXME (kmabee) consider sub_device_ids added recently.
     detail::DispatchStateCheck(true);
     cq.run_command(CommandInterface{.type = EnqueueCommandType::FINISH, .blocking = true, .sub_device_ids = sub_device_ids});
     TT_ASSERT(
@@ -2132,7 +2136,7 @@ void Finish(CommandQueue& cq, tt::stl::Span<const SubDeviceId> sub_device_ids) {
 }
 
 void EnqueueTrace(CommandQueue& cq, uint32_t trace_id, bool blocking) {
-    TRACE_FUNCTION_CALL(captureEnqueueTrace, cq, trace_id, blocking);
+    TRACE_FUNCTION_CALL(CaptureEnqueueTrace, cq, trace_id, blocking);
     detail::DispatchStateCheck(true);
     TT_FATAL(cq.device()->get_trace(trace_id) != nullptr, "Trace instance {} must exist on device", trace_id);
     cq.run_command(
