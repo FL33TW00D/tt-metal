166628437ba32e63ba20857754a8107b172d5fdf
Paul Keller <pkeller@tenstorrent.com>
2025-01-07 21:00:37 +0000

    more flexible

--- tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp
+++ tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp
@@ -6,27 +6,40 @@
 
 constexpr uint32_t page_size = get_compile_time_arg_val(0);
 constexpr uint32_t pages = get_compile_time_arg_val(1);
-constexpr uint32_t width = get_compile_time_arg_val(2);
-constexpr uint64_t duration = (uint64_t)get_compile_time_arg_val(3) * 1000 * 1000 * 1000;
-constexpr uint32_t ucast_size = (uint64_t)get_compile_time_arg_val(4);
-constexpr uint32_t mcast_size = (uint64_t)get_compile_time_arg_val(5);
+constexpr uint32_t tlx = get_compile_time_arg_val(2);
+constexpr uint32_t tly = get_compile_time_arg_val(3);
+constexpr uint32_t mcaster = get_compile_time_arg_val(4);
+constexpr uint32_t width = get_compile_time_arg_val(5);
+constexpr uint32_t height = get_compile_time_arg_val(6);
+constexpr uint32_t ucast_v = get_compile_time_arg_val(7);
+constexpr uint64_t duration = (uint64_t)get_compile_time_arg_val(8) * 1000 * 1000 * 1000;
+constexpr uint32_t ucast_size = (uint64_t)get_compile_time_arg_val(9);
+constexpr uint32_t mcast_size = (uint64_t)get_compile_time_arg_val(10);
 
 void kernel_main() {
-    cb_reserve_back(0, 1);
-    uint32_t cb_addr = get_write_ptr(0);
+    uint32_t cb_addr = 200000;  // get_write_ptr(0);
 
     uint64_t done_time = c_tensix_core::read_wall_clock() + duration;
-    bool mcast = (my_x[NOC_INDEX] == 0);
+
+    uint64_t dst_x, dst_y;
+    if (ucast_v) {
+        dst_x = my_x[NOC_INDEX];
+        dst_y = (my_y[NOC_INDEX] == tly) ? tly + height - 1 : my_y[NOC_INDEX] - 1;
+    } else {
+        dst_x = (my_x[NOC_INDEX] == tlx + width - 1) ? tlx : my_x[NOC_INDEX] + 1;
+        dst_y = my_y[NOC_INDEX];
+    }
+
     while (c_tensix_core::read_wall_clock() < done_time) {
         for (uint32_t count = 0; count < 1000; count++) {
             uint32_t read_ptr = cb_addr;
             uint32_t write_ptr = cb_addr;
-            if (mcast) {
+            if (mcaster) {
                 uint64_t dst_noc_multicast_addr =
-                    get_noc_multicast_addr(2, my_y[NOC_INDEX], width + 1, my_y[NOC_INDEX], write_ptr);
-                noc_async_write_multicast(read_ptr, dst_noc_multicast_addr, mcast_size, width, false);
+                    get_noc_multicast_addr(tlx, tly, tlx + width - 1, tly + height - 1, write_ptr);
+                noc_async_write_multicast(read_ptr, dst_noc_multicast_addr, mcast_size, width * height, false);
             } else {
-                uint64_t noc_write_addr = NOC_XY_ADDR(NOC_X(my_x[NOC_INDEX] + 1), NOC_Y(my_y[NOC_INDEX]), write_ptr);
+                uint64_t noc_write_addr = NOC_XY_ADDR(NOC_X(dst_x), NOC_Y(dst_y), write_ptr);
                 noc_async_write(read_ptr, noc_write_addr, ucast_size);
             }
         }
--- tests/tt_metal/tt_metal/test_stress_noc_mcast.cpp
+++ tests/tt_metal/tt_metal/test_stress_noc_mcast.cpp
@@ -25,75 +25,126 @@ const uint32_t PAGE_SIZE = CB_ELEMENTS * sizeof(float);
 const uint32_t PAGES = 2;
 const uint32_t DEFAULT_SECONDS = 10;
 const uint32_t DEFAULT_TARGET_WIDTH = 1;
+const uint32_t DEFAULT_TARGET_HEIGHT = 1;
 
 uint32_t device_num_g = 0;
 uint32_t duration_secs_g = DEFAULT_SECONDS;
 uint32_t width_g = DEFAULT_TARGET_WIDTH;
+uint32_t height_g = DEFAULT_TARGET_HEIGHT;
+uint32_t tlx_g = 1;
+uint32_t tly_g = 0;
+uint32_t mcast_x_g = 0;
+uint32_t mcast_y_g = 0;
+bool ucast_v_g = false;
 uint32_t mcast_size_g = 16;
 uint32_t ucast_size_g = 8192;
-uint32_t start_row_g = 0;
-uint32_t rows_g = 1;
+bool mcast_from_eth_g = false;
 
 void init(int argc, char** argv) {
     std::vector<std::string> input_args(argv, argv + argc);
 
     if (test_args::has_command_option(input_args, "-h") || test_args::has_command_option(input_args, "--help")) {
         log_info(LogTest, "Usage:");
-        log_info(LogTest, "  -v: device number to run on (default 0), ", DEFAULT_SECONDS);
-        log_info(LogTest, "  -d: duration in seconds (default {}), ", DEFAULT_SECONDS);
-        log_info(LogTest, "  -w: target core width (default {}), ", DEFAULT_TARGET_WIDTH);
+        log_info(LogTest, "  -v: device number to run on (default 0) ", DEFAULT_SECONDS);
+        log_info(LogTest, "  -d: duration in seconds (default {})", DEFAULT_SECONDS);
+        log_info(LogTest, "  -width: unicast grid width (default {})", DEFAULT_TARGET_WIDTH);
+        log_info(LogTest, "  -height: unicast grid height (default {})", DEFAULT_TARGET_HEIGHT);
+        log_info(LogTest, "-tlx: grid top left x");
+        log_info(LogTest, "-tly: grid top left y");
+        log_info(LogTest, " -mx: mcast core x");
+        log_info(LogTest, " -my: mcast core y");
+        log_info(LogTest, "  -l: unicast vertically (default horizontally)");
+        log_info(LogTest, "  -e: mcast from eth core");
         log_info(LogTest, "  -m: mcast packet size");
         log_info(LogTest, "  -u: ucast packet size");
-        log_info(LogTest, " -sr: start row");
-        log_info(LogTest, "  nr: rows");
         exit(0);
     }
 
     device_num_g = test_args::get_command_option_uint32(input_args, "-v", 0);
     duration_secs_g = test_args::get_command_option_uint32(input_args, "-d", DEFAULT_SECONDS);
-    width_g = test_args::get_command_option_uint32(input_args, "-w", DEFAULT_TARGET_WIDTH);
+    width_g = test_args::get_command_option_uint32(input_args, "-width", DEFAULT_TARGET_WIDTH);
+    height_g = test_args::get_command_option_uint32(input_args, "-height", DEFAULT_TARGET_HEIGHT);
+    tlx_g = test_args::get_command_option_uint32(input_args, "-tlx", 0);
+    tly_g = test_args::get_command_option_uint32(input_args, "-tly", 0);
+    mcast_x_g = test_args::get_command_option_uint32(input_args, "-mx", 0);
+    mcast_y_g = test_args::get_command_option_uint32(input_args, "-my", 0);
     mcast_size_g = test_args::get_command_option_uint32(input_args, "-m", 16);
     ucast_size_g = test_args::get_command_option_uint32(input_args, "-u", 8192);
-    start_row_g = test_args::get_command_option_uint32(input_args, "-sr", 0);
-    rows_g = test_args::get_command_option_uint32(input_args, "-r", 1);
+    ucast_v_g = test_args::has_command_option(input_args, "-l");
+    mcast_from_eth_g = test_args::has_command_option(input_args, "-e");
+
+    if (!mcast_from_eth_g && mcast_x_g >= tlx_g && mcast_x_g <= tlx_g + width_g - 1 && mcast_y_g >= tly_g &&
+        mcast_y_g <= tly_g + height_g - 1) {
+        log_fatal("Mcast core can't be within mcast grid");
+        exit(-1);
+    }
 }
 
 int main(int argc, char** argv) {
     init(argc, argv);
 
     tt_metal::Device* device = tt_metal::CreateDevice(device_num_g);
-    CommandQueue& cq = device->command_queue();
     tt_metal::Program program = tt_metal::CreateProgram();
 
-    CoreRange workers({0, start_row_g}, {width_g, start_row_g + rows_g - 1});  // right column does no work
+    CoreRange workers_logical({tlx_g, tly_g}, {tlx_g + width_g - 1, tly_g + height_g - 1});
+    CoreCoord mcast_logical(mcast_x_g, mcast_y_g);
+    CoreCoord tl_core = device->worker_core_from_logical_core({tlx_g, tly_g});
+    CoreCoord mcast_core = device->worker_core_from_logical_core(mcast_logical);
     std::vector<uint32_t> compile_args = {
         PAGE_SIZE,
         PAGES,
+        tl_core.x,
+        tl_core.y,
+        false,
         width_g,
+        height_g,
+        ucast_v_g,
         duration_secs_g,
         ucast_size_g,
         mcast_size_g,
     };
 
-    tt_metal::CircularBufferConfig cb_config =
-        tt_metal::CircularBufferConfig(PAGE_SIZE * PAGES, {{0, tt::DataFormat::Float32}}).set_page_size(0, PAGE_SIZE);
-    auto cb = tt_metal::CreateCircularBuffer(program, workers, cb_config);
-
-    auto dm0 = tt_metal::CreateKernel(
+    tt_metal::CreateKernel(
         program,
         "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
-        workers,
+        workers_logical,
         tt_metal::DataMovementConfig{
             .processor = tt_metal::DataMovementProcessor::RISCV_0,
             .noc = tt_metal::NOC::RISCV_0_default,
             .compile_args = compile_args,
         });
 
-    log_info(LogTest, "Running on cores: {}", workers.str());
-    log_info(LogTest, "MCast core(s) writing {} bytes per xfer", mcast_size_g);
-    log_info(LogTest, "UCast core(s) writing {} bytes per xfer", ucast_size_g);
+    compile_args[4] = true;
+    if (mcast_from_eth_g) {
+        tt_metal::CreateKernel(
+            program,
+            "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
+            mcast_logical,
+            tt_metal::EthernetConfig{
+                .noc = tt_metal::NOC::NOC_0,
+                .compile_args = compile_args,
+            });
+    } else {
+        tt_metal::CreateKernel(
+            program,
+            "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
+            mcast_logical,
+            tt_metal::DataMovementConfig{
+                .processor = tt_metal::DataMovementProcessor::RISCV_0,
+                .noc = tt_metal::NOC::RISCV_0_default,
+                .compile_args = compile_args,
+            });
+    }
+
+    log_info(LogTest, "MCast core: {}, writing {} bytes per xfer", mcast_logical, mcast_size_g);
+    log_info(
+        LogTest,
+        "Unicast grid: {}, writing {} bytes per xfer {}",
+        workers_logical.str(),
+        ucast_size_g,
+        ucast_v_g ? "bottom to top" : "left to right");
     log_info(LogTest, "Running for {} seconds", duration_secs_g);
 
-    EnqueueProgram(cq, program, true);
+    tt::tt_metal::detail::LaunchProgram(device, program, true);
     tt_metal::CloseDevice(device);
 }
